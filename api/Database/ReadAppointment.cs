c8e3edf1040e040c862c3ac574e084b1a4db1012
Spencer Audet <sdaudet@crimson.ua.edu>
2021-04-14 13:16:34 -0500

    Got it

--- api/Database/ReadAppointment.cs
+++ api/Database/ReadAppointment.cs
@@ -25,21 +25,14 @@ namespace api.Database{
                 return new Appointment(){appointmentId=rdr.GetInt32(1),appointmentCustomer=readCust.GetCustomerByID(rdr.GetInt32(2)),appointmentTrainer=readTrn.GetTrainerByID(rdr.GetInt32(3)),appointmentActivity=readAct.Read(rdr.GetInt32(4)), startTime=rdr.GetDateTime(5), endTime=rdr.GetDateTime(6),appointmentCost=(rdr.GetDouble(7)),amountPaidByCash=rdr.GetDouble(8), amountPaidByCard=rdr.GetDouble(9)};
             }
             return null;        
-<<<<<<< HEAD
-            }
-        public List<Appointment> GetCustAvailableAppointmentsByDate(DateTime searchDate){
-=======
         }
         public List<Appointment> ReadAll(){
->>>>>>> spencer
             List<Appointment> returnList = new List<Appointment>();
             ConnectionString cs = new ConnectionString();
             using var con = new MySqlConnection(cs.cs);
             con.Open();
             using var cmd = new MySqlCommand();
             cmd.Connection = con;
-<<<<<<< HEAD
-=======
             cmd.CommandText = @"SELECT AppointmentID,CustomerID,a.TrainerID,a.ActivityID,starttime,endtime,Price,CashAmount,CardAmount FROM Appointment a JOIN Cando c on a.TrainerID=c.TrainerID AND a.activityID=c.activityID";
             cmd.Prepare();
             cmd.ExecuteReader();
@@ -52,14 +45,13 @@ namespace api.Database{
             }
             return returnList;
             }
-        public List<Appointment> GetCustAvailableAppointmentsByDate(DateTime searchDate){
+        public List<Appointment> ReadAvailableAppointmentsByDate(DateTime searchDate){
             List<Appointment> returnList = new List<Appointment>();
             ConnectionString cs = new ConnectionString();
             using var con = new MySqlConnection(cs.cs);
             con.Open();
             using var cmd = new MySqlCommand();
             cmd.Connection = con;
->>>>>>> spencer
             cmd.CommandText = @"SELECT AppointmentID,CustomerID,a.TrainerID,a.ActivityID,starttime,endtime,Price,CashAmount,CardAmount FROM Appointment a JOIN Cando c on a.TrainerID=c.TrainerID AND a.activityID=c.activityID WHERE CustomerID=null AND starttime LIKE @date";
             cmd.Parameters.AddWithValue("@date",searchDate);
             cmd.Prepare();
@@ -74,6 +66,37 @@ namespace api.Database{
             return returnList;
             }
 
+        public List<Appointment> ReadDistinctAvailableAppointments()
+        {
+            List<Appointment> myAppointments = new List<Appointment>();
+            ConnectionString conStr = new ConnectionString();
+            string cs = conStr.cs;
+            //Open the connection
+            using var con = new MySqlConnection(cs);
+            con.Open();
+
+            //I (Gabby) commented these lines out and updated lines 52-53 instead because there were errors
+            // cmd.Connection = con;
+            // cmd.CommandText = @"SELECT DISTINCT AppointmentDate,AppointmentID,CustomerID,a.TrainerID,a.ActivityID,starttime,endtime,Price FROM Appointment a JOIN Cando c on a.TrainerID=c.TrainerID AND a.activityID=c.activityID WHERE CustomerId = null ORDER BY ___ asc";
+            
+            //Statement
+            string stm = @"SELECT DISTINCT AppointmentDate,AppointmentID,CustomerID,a.TrainerID,a.ActivityID,starttime,endtime,Price FROM Appointment a JOIN Cando c on a.TrainerID=c.TrainerID AND a.activityID=c.activityID WHERE CustomerId = null ORDER BY ___ asc";
+            using var cmd = new MySqlCommand(stm, con);
+            using MySqlDataReader rdr = cmd.ExecuteReader();
+
+            //created new ReadCustomer, ReadTrainer, and ReacActivity to get Customer, Trainer, and Activity data
+            IReadCustomer readCust = new ReadCustomer();
+            IReadTrainer readTrn = new ReadTrainer();
+            IReadActivity readAct = new ReadActivity();
+            while (rdr.Read())
+            {
+                Appointment temp = new Appointment(){appointmentId=rdr.GetInt32(1),appointmentCustomer=readCust.GetCustomerByID(rdr.GetInt32(2)),appointmentTrainer=readTrn.GetTrainerByID(rdr.GetInt32(3)),appointmentActivity=readAct.Read(rdr.GetInt32(4)), startTime=rdr.GetDateTime(5), endTime=rdr.GetDateTime(6),appointmentCost=(rdr.GetDouble(7)),amountPaidByCash=rdr.GetDouble(8), amountPaidByCard=rdr.GetDouble(9)};
+                myAppointments.Add(temp);
+
+            }
+            return myAppointments;
+        }
+             
         public List<Appointment> ReadAvailableAppointmentsByDateForTrainer(int trainerId, DateTime date)
         {
             List<Appointment> myAppointments = new List<Appointment>();
