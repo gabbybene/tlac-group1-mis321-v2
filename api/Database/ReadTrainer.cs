1a83a815b7150c424028e66afc734e4045125954
Gabby Benefield <GabbyMoody@Gabrielles-iMac.local>
2021-04-15 21:09:55 -0500

    Can now Create New Trainer & Log In

--- api/Database/ReadTrainer.cs
+++ api/Database/ReadTrainer.cs
@@ -6,21 +6,23 @@ using MySql.Data.MySqlClient;
 namespace api.Database{
     public class ReadTrainer : IReadTrainer
     {
-        public models.Trainer Read(string emailAddress){
+        public Trainer Read(string emailAddress){
             ConnectionString cs = new ConnectionString();
             using var con = new MySqlConnection(cs.cs);
             con.Open();
             using var cmd = new MySqlCommand();
             cmd.Connection = con;
-            cmd.CommandText = @"SELECT (TrainerID,fName,lName,DOB,Gender,AcctID) FROM Trainer WHERE AcctID=@AcctID";
+            cmd.CommandText = @"SELECT * FROM Trainer t JOIN Account a ON t.AcctID=a.AcctID WHERE a.AcctID=@AcctID";
             cmd.Parameters.AddWithValue("@AcctID",emailAddress);
             cmd.Prepare();
-            cmd.ExecuteReader();
+            
+            Trainer trainer = new Trainer();
             MySqlDataReader rdr = cmd.ExecuteReader();
-            if (rdr.Read()){
-                return new Trainer(){trainerId=rdr.GetInt32(1),fName=rdr.GetString(2),lName=rdr.GetString(3),birthDate=rdr.GetDateTime(4), gender=rdr.GetString(5), email=rdr.GetString(6)};
+            while (rdr.Read()){
+                Trainer temp = new Trainer(){trainerId=rdr.GetInt32(0),fName=rdr.GetString(1),lName=rdr.GetString(2),birthDate=rdr.GetDateTime(3), gender=rdr.GetString(4), email=rdr.GetString(5), password=rdr.GetString(6)};
+                trainer = temp;
             }
-            return null;
+            return trainer;
         }
         public Trainer GetTrainerByID(int id){
             ConnectionString cs = new ConnectionString();
--- api/Database/WriteTrainer.cs
+++ api/Database/WriteTrainer.cs
@@ -13,15 +13,18 @@ namespace api.Database
             con.Open();
             using var cmd = new MySqlCommand();
 
-            cmd.CommandText = @"INSERT into account (Email,Password,AcctType) VALUES (@email,@password,'trainer');
-                            INSERT into trainer (fname, lname, DOB, gender, acctID, phone) VALUES (@fname,@lname,@dob,@gender, (SELECT acctID FROM Account WHERE acctID=@email), @phone)";
+            cmd.CommandText = @"INSERT into account (AcctID,Password,AcctType) VALUES (@email, @password,'customer')"; 
             cmd.Parameters.AddWithValue("@email", i.email);
+            cmd.Parameters.AddWithValue("@password", i.password);
+            cmd.Connection=con;
+            cmd.Prepare();
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = @"INSERT into trainer (fName, lName, DOB, Gender, AcctID) VALUES (@fname,@lname,@dob,@gender, (SELECT acctID FROM Account WHERE acctID=@email))";
             cmd.Parameters.AddWithValue("@fname", i.fName);
             cmd.Parameters.AddWithValue("@lname", i.lName);
             cmd.Parameters.AddWithValue("@dob", i.birthDate);
             cmd.Parameters.AddWithValue("@gender", i.gender);
-            cmd.Parameters.AddWithValue("@phone", i.phoneNo);
-            cmd.Parameters.AddWithValue("@password", i.password);
             cmd.Connection=con;
             cmd.Prepare();
             cmd.ExecuteNonQuery();
